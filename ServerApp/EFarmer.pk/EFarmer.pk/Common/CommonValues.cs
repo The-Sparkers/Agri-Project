619cbe7fb10b60fdd20aafc695c0b014a5e5cf4f
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2019-12-15 21:48:11 +0500

    Setup server app

---
+++ ServerApp/EFarmer.pk/EFarmer.pk/Common/CommonValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFarmer.pk.Common
+{
+    public static class CommonValues
+    {
+        public static readonly string APP_NAME = "e-Farmer";
+        public static readonly string APP_NAME_PART = ".pk";
+    }
+}
