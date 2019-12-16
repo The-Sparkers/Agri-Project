0ca189e5e3607267574e84e1059ceb24a09de940
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2019-12-16 17:16:47 +0500

    Added GeoLocation Class

--- ServerApp/EFarmer.pk/EFarmer.pk/Common/CommonValues.cs
+++ ServerApp/EFarmer.pk/EFarmer.pk/Common/CommonValues.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Configuration;
 namespace EFarmer.pk.Common
 {
     public static class CommonValues
     {
         public static readonly string APP_NAME = "e-Farmer";
         public static readonly string APP_NAME_PART = ".pk";
+        public static readonly string CONNECTION_STRING;
+        static CommonValues()
+        {
+            string projectPath = AppDomain.CurrentDomain.BaseDirectory.Split(new String[] { @"bin\" }, StringSplitOptions.None)[0];
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(projectPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            //CONNECTION_STRING = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            CONNECTION_STRING = configuration.GetConnectionString("DefaultConnection");
+        }
     }
 }
