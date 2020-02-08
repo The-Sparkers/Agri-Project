e41c2db308ae9063c27e0f50c313a95ea212855f
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2020-02-08 16:32:39 +0500

    Created Repository Factory

--- ServerApp/EFarmer.pk/EntityModelLibrary/Connections/SQLConnection.cs
+++ ServerApp/EFarmer.pk/EntityModelLibrary/Connections/SQLConnection.cs
@@ -5,37 +5,12 @@ using System.Runtime.Serialization;
 namespace EFarmer.Connections
 {
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-    [DataContract]
-    public abstract class SQLConnection
-    {
-        public SQLConnection(IDbConnection connection)
-        {
-            Connection = new SqlConnection(connection.GetConnectionString());
-            StoredProcedureCommand = new SqlCommand
-            {
-                Connection = Connection,
-                CommandType = System.Data.CommandType.StoredProcedure
-            };
-            QueryCommand = new SqlCommand
-            {
-                Connection = Connection
-            };
-        }
 
-        protected SqlConnection Connection { get; private set; }
-        protected SqlCommand StoredProcedureCommand { get; private set; }
-        protected SqlCommand QueryCommand { get; private set; }
-        public void ResetCommands()
+    public class SQLConnection : DbConnection
+    {
+        public SQLConnection(string connectionString) : base(connectionString)
         {
-            StoredProcedureCommand = new SqlCommand()
-            {
-                CommandType = System.Data.CommandType.StoredProcedure,
-                Connection = Connection
-            };
-            QueryCommand = new SqlCommand()
-            {
-                Connection = Connection
-            };
+
         }
     }
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
---
+++ ServerApp/EFarmer.pk/EntityModelLibrary/Factories/RepositoryFactory.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using Autofac.Core;
+using EFarmer.Connections;
+using EFarmerPkModelLibrary.Repositories;
+using EntityGrabber;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFarmerPkModelLibrary.Factories
+{
+    public class RepositoryFactory
+    {
+        private readonly BuildOptions options;
+        public RepositoryFactory(BuildOptions options)
+        {
+            this.options = options;
+        }
+        public IContainer Build()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<SQLConnection>()
+                .As<IDbConnection>()
+                .WithParameter("connectionString", options.ConnectionString);
+            builder.RegisterType<UserRepository>()
+                .As<IUserRepository>();
+            builder.RegisterType<CityRepository>()
+                .As<ICityRepository>();
+
+            return builder.Build();
+        }
+        public class BuildOptions
+        {
+            public string ConnectionString { get; set; }
+        }
+    }
+}
